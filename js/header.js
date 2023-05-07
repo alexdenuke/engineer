4c40f151b15556494b42369ba3bf782bc5c1a38e
alexdenuke <alexdenuke@gmail.com>
2023-05-07 03:41:50 +0300

    add burger

---
+++ js/header.js
@@ -0,0 +1,43 @@
+let link = document.querySelector('.link-anchor');
+let mainBtn = document.querySelector('.main-title-btn');
+let links = document.querySelectorAll('.header__nav-link[href^="#"]');
+let burger = document.querySelector('.burger-img');
+let burgerBody = document.querySelector('.burger');
+let burgerlinks = document.querySelectorAll('.burger-link[href^="#"]');
+
+const linksArr = [link, mainBtn];
+linksArr.push(...links, ...burgerlinks);
+
+for (let i = 0; i < linksArr.length; i++) {
+  linksArr[i].addEventListener('click', handleClick);
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  burgerBody.classList.remove('burger-active');
+
+  let targetId = this.getAttribute('href');
+  let targetElement = document.querySelector(targetId);
+  let headerHeight = document.querySelector('header').offsetHeight;
+  let offset = targetElement.offsetTop - headerHeight;
+
+  window.scrollTo({
+    top: offset,
+    behavior: 'smooth'
+  });
+}
+
+burger.addEventListener('click', () => {
+  burgerBody.classList.toggle('burger-active');
+})
+
+
+
+
+
+
+
+
+
+
+
--- js/swiper.js
+++ js/swiper.js
@@ -21,17 +21,27 @@ new Swiper(slider, {
   new Swiper(slider2, {
     direction: 'horizontal',
     // loop: true,
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     // pagination: {
     //   el: '.swiper-pagination',
     //   type: 'fraction',
     //   clickable: true,
     // },
     // slidesPerGroup: 1,
-    // spaceBetween: 50,
-    slidesPerView: 3,
+    spaceBetween: 30,
     grabCursor: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      500: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      }
+    }
   });
